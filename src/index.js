c68766b3ad4834544ec7fea461a72821540e8fe9
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-21 00:16:56 +0300

    feat: add library

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+  function toReadable(number) {
+    const chars = {
+      ones: ['zero', 'one', 'two, three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+      tens: ['eleven', 'twelve', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+      hundreds: ['hundred'],
+    };
+    let toArray = number.toString().split('');
+
+    switch (toArray.length) {
+      case 3:
+
+      case 2:
+
+      case 1:
+    }
+
+    return toArray;
+  }
 }
