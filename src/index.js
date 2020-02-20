9361058af9374641857a9d7bb8abcb8fc0f9e8d6
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-21 00:54:32 +0300

    refactor: fix problem with tens

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 module.exports = function toReadable (number) {
   const chars = {
-    ones: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
-    tens: ['eleven', 'twelve', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+    tens: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'forteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
     hundreds: ['hundred'],
   };
 
   let toArray = number.toString().split('');
   let toRead = '';
+  let tens = [];
 
   switch (toArray.length) {
     case 3:
+      tens = toArray[1].concat(toArray[2]);
       toRead = `${chars.ones[toArray[0]]} ${chars.hundreds} ${chars.tens[toArray[1]]} ${chars.ones[toArray[2]]}`;
       break;
     case 2:
-      toRead = `${chars.tens[toArray[0]]} ${chars.ones[toArray[1]]}`;
+      tens = toArray[0].concat(toArray[1]);
+      toRead = `${chars.tens[number]}`;
       break;
     case 1:
-      toRead = `${chars.ones[toArray[0]]}`;
+      toRead = `${chars.tens[number]}`;
       break;
   }
 
