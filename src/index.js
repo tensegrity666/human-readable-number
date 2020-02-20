da73cd045b595b4bde4cf631cc9f850e9a6cf87e
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-21 00:28:49 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 module.exports = function toReadable (number) {
-  function toReadable(number) {
-    const chars = {
-      ones: ['zero', 'one', 'two, three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
-      tens: ['eleven', 'twelve', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
-      hundreds: ['hundred'],
-    };
-    let toArray = number.toString().split('');
+  const chars = {
+    ones: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+    tens: ['eleven', 'twelve', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+    hundreds: ['hundred'],
+  };
 
-    switch (toArray.length) {
-      case 3:
+  let toArray = number.toString().split('');
+  let toRead = '';
 
-      case 2:
-
-      case 1:
-    }
-
-    return toArray;
+  switch (toArray.length) {
+    case 3:
+      toRead = `${chars.ones[toArray[0]]} ${chars.hundreds} ${chars.tens[toArray[1]]} ${chars.ones[toArray[2]]}`;
+      break;
+    case 2:
+      toRead = `${chars.tens[toArray[0]]} ${chars.ones[toArray[1]]}`;
+      break;
+    case 1:
+      toRead = `${chars.ones[toArray[0]]}`;
+      break;
   }
+
+  return toRead;
 }
